340d59b2206d508e741e62b73efb40712c41a935
Louie - work <codelouie94@gmail.com>
2022-12-22 19:46:25 -0600

    sum of positives

---
+++ sumOfPositive.js
@@ -0,0 +1,14 @@
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+    let positiveArray = arr.filter(num => num > 0)
+    let result = 0
+    for (let i of positiveArray) {
+        result += i
+    }
+    return result
+}
