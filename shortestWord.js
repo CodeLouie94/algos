06be16e3e14e4f7092ee772cf88244188014f20b
Louie - work <codelouie94@gmail.com>
2023-01-02 02:01:05 -0600

    shortest word

---
+++ shortestWord.js
@@ -0,0 +1,16 @@
+/*
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+ */
+
+function findShort(s) {
+    let arr = s.split(" ")
+    let smallestLength = arr[0].length
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i].length < smallestLength) {
+            smallestLength = arr[i].length
+        }
+    }
+    return smallestLength
+}
